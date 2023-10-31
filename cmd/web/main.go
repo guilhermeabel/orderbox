5086a74804b823ac73f0aa33e9f8bbaa2ad696c7
Guilherme Abel <guilhermeabel8@gmail.com>
2023-10-30 21:02:44 -0300

    feat: config and logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,24 +1,41 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+type config struct {
+	addr string
+}
+
+var cfg config
 
 func main() {
 
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
-
 	fileServer := http.FileServer(http.Dir("../ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/order/show", showOrder)
 	mux.HandleFunc("/order/create", createOrder)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     cfg.addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", cfg.addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 
 }
