69235310ffbe7a2d613a909fb1c393842fae689b
Guilherme Abel <guilhermeabel8@gmail.com>
2023-11-14 22:02:51 -0300

    wip: database driver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
@@ -21,10 +22,18 @@ type application struct {
 func main() {
 
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/orderbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -38,7 +47,18 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", cfg.addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/guilhermeabel/restaurant-ordering-system
 
 go 1.20
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
