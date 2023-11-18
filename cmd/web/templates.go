eda297593f1547e9307a2cfec4ea5b7725bc97f3
Guilherme Abel <guilhermeabel8@gmail.com>
2023-11-18 18:46:56 -0300

    feat: func map in templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"path/filepath"
 	"text/template"
+	"time"
 
 	"github.com/guilhermeabel/orderbox/internal/models"
 )
@@ -11,6 +12,14 @@ type templateData struct {
 	Order       *models.Order
 	Orders      []*models.Order
 	CurrentYear int
+}
+
+var functions = template.FuncMap{
+	"parseDate": parseDate,
+}
+
+func parseDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -24,7 +33,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("../ui/html/base.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("../ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}
