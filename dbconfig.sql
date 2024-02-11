9ff8b113aba2e0e817aa3dd35ecce3984c4840c6
Guilherme Abel <guilhermeabel8@gmail.com>
2024-02-11 14:38:25 -0300

    dev: users table

--- dbconfig.sql
+++ dbconfig.sql
@@ -53,3 +53,14 @@ CREATE TABLE sessions (
 );
 
 CREATE INDEX sessions_expiry_idx ON sessions (expiry);
+
+-- Users table
+CREATE TABLE users (
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	hashed_password CHAR(60) NOT NULL,
+	created DATETIME NOT NULL
+);
+
+ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
