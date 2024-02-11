3b18ff19e72ca15d31f1d2916ee6a59bf421b1fb
Guilherme Abel <guilhermeabel8@gmail.com>
2024-02-11 14:38:45 -0300

    feat: users routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,6 +23,12 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/order/create", dynamic.ThenFunc(app.createOrder))
 	router.Handler(http.MethodPost, "/order/create", dynamic.ThenFunc(app.createOrderPost))
 
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
+	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
+	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	return standard.Then(router)
 }
