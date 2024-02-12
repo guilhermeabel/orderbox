400d6a4983e0f59f961fc8e82d3e3bff55570da6
Guilherme Abel <guilhermeabel8@gmail.com>
2024-02-12 16:06:21 -0300

    dev: using FS to manage files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/guilhermeabel/orderbox/cmd/ui"
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
@@ -13,8 +14,8 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("../ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"io/fs"
 	"path/filepath"
 	"text/template"
 	"time"
 
+	"github.com/guilhermeabel/orderbox/cmd/ui"
 	"github.com/guilhermeabel/orderbox/internal/models"
 )
 
@@ -29,7 +31,7 @@ func parseDate(t time.Time) string {
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("../ui/html/pages/*.html")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.html")
 	if err != nil {
 		return nil, err
 	}
@@ -37,17 +39,13 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("../ui/html/base.html")
-		if err != nil {
-			return nil, err
+		patterns := []string{
+			"html/base.html",
+			"html/components/*.html",
+			page,
 		}
 
-		ts, err = ts.ParseGlob("../ui/html/components/*.html")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
